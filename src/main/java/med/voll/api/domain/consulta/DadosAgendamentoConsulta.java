6766a3e91c0517d54f6066effdeeb41004a30916
Lucas Robli <lucasroblim@gmail.com>
2023-05-29 21:54:28 -0300

    criação da entidade, controllers, dtos e também criação do service para as regras de negocio

---
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+
+        Long idMedico,
+
+        @NotNull
+        Long idPaciente,
+
+        @NotNull
+        @Future //garante que a data informada não seja anterior a data e hora atual
+        LocalDateTime data
+) {
+
+
+}
